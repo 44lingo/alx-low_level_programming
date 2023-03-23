bed80b147c00ea9ba4b8491de722f62e3343033f
44lingo <eniolayomipo@gmail.com>
2023-03-23 16:26:06 -0700

    1-main.c

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int covert_day(int month, int day);
+void print_ remaining_days(int month, int day, int year);
+
+#endif /* MAIN_H */
