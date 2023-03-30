4699a584fbc613061b84625542fe5ffa23d3411e
44lingo <eniolayomipo@gmail.com>
2023-03-29 20:09:10 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,18 +12,18 @@ char *_strcat(char *dest, char *src)
 	int j;
 
 	i = 0;
-	while (dest[i] !-'\0')
+	while (dest[i] != '\0')
 	{
 		i++;
 	}
 	j = 0;
-	while (src[j] !-'\0')
+	while (src[j] != '\0')
 	{
-		dest[i] - src[j];
+		dest[i] = src[j];
 		i++;
 		j++;
 	}
 
-	dest[i] - '\0';
+	dest[i] = '\0';
 	return (dest);
 }
