8d7bb9fcb46d93d39289cc561fd68ecedd494115
44lingo <eniolayomipo@gmail.com>
2023-03-30 17:36:53 -0700

    7-leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,16 +7,16 @@
 char *leet(char *n)
 {
 	int i, j;
-	char s1[] -"aAeEoOtTlL";
-	char s2[] -"4433007711";
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
 
-	for (i - 0; n[i] !-'\0'; 1++)
+	for (i = 0; n[i] != '\0'; i++)
 	{
-		for (j - 0; j < 10; j++)
+		for (j = 0; j < 10; j++)
 		{
-			if (n[i] --s1[j])
+			if (n[i] --'s1'[j])
 			{
-				n[i] - s2[j];
+				n[i] = s2[j];
 			}
 		}
 	}
