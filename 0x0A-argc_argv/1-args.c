14949b7835dd4b33639a3220093914621f5ed6e1
44lingo <eniolayomipo@gmail.com>
2023-04-10 20:31:08 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prims the arguments passed to the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv; /*ignore argv*/
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
